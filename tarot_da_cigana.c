f58f9ec63a4a3358930576a8a2bfadfdded2bb9a
gcileno <gabriel.cileno.093@ufrn.edu.br>
2021-10-20 18:43:01 -0300

    novas telas
    adcionei novamente novas telas

--- tarot_da_cigana.c
+++ tarot_da_cigana.c
@@ -1,9 +1,8 @@
 // Assinatura das funções
-void  telaPrincipal ( void );
-void  telaEquipe ( void );
-void  cabecalho(void);
+void telaPrincipal ( void );
+void telaInfo ( void );
+void cabecalho(void);
 void cadastro (void);
-void saudacao (void);
 
 // ///
 // Programa principal
@@ -18,50 +17,48 @@ int  main ( void ) {
 
 
 void  telaPrincipal ( void ) {
-    printf ( " ///////////////////////////////////////////////// ////////////////////////////////////// \ n " );
-    printf ( " /// /// \ n " );
-    printf ( " /// = = = = = Sistema Tarot da Cigana Alzira = = = = = /// \ n " );
-    printf ( " /// /// \ n " );
-    printf ( " /// 1. Cadastro do jogado /// \ n " );
-    printf ( " /// 2. Jogar cartas /// \ n " );
-    printf ( " /// 3. Jogar cartas sem cadastro /// \ n " );
-    printf ( " /// 0. Sair /// \ n " );
-    printf ( " /// /// \ n " );
-    printf ( " ///////////////////////////////////////////////// ////////////////////////////////////// \ n " );
-    printf ( " \ n " );
+    printf ( " ///////////////////////////////////////////////// ////////////////////////////////////// \n " );
+    printf ( " /// /// \n " );
+    printf ( " /// = = = = = Sistema Tarot da Cigana Alzira = = = = = /// \n " );
+    printf ( " /// /// \n " );
+    printf ( " /// 1. Cadastro do jogado /// \n " );
+    printf ( " /// 2. Jogar cartas /// \n " );
+    printf ( " /// 3. Jogar cartas sem cadastro /// \n " );
+    printf ( " /// 0. Sair /// \n " );
+    printf ( " /// /// \n " );
+    printf ( " ///////////////////////////////////////////////// ////////////////////////////////////// \n " );
+    printf ( " \n " );
 }
-
-
 
 void  telaInfo ( void ) {
     printf ( " ///////////////////////////////////////////////// ////////////////////////////////////// \ n " );
-    printf ( " ///                                                                                  /// \ n " );
-    printf ( " /// Universidade Federal do Rio Grande do Norte /// \ n " );
-    printf ( " /// Centro de Ensino Superior do Seridó /// \ n " );
-    printf ( " /// Departamento de Computação e Tecnologia /// \ n " );
-    printf ( " /// Disciplina DCT1106 - Programação /// \ n " );
-    printf ( " /// Projeto Tarot da Cigana Alzira /// \ n " );
-    printf ( " /// Lecionado por @flgorgonio - Out, 2021 /// \ n " );
-    printf ( " /// Desenvolvido por @gcileno & @Gabriel5z - Out, 2021 /// \ n " );
     printf ( " ///                                                                                  /// \n " );
-    printf ( " /// = = = = = Sistema Tarot da Cigana Alzira = = = = = /// \ n " );
+    printf ( " /// Universidade Federal do Rio Grande do Norte                                      /// \n " );
+    printf ( " /// Centro de Ensino Superior do Seridó                                              /// \n " );
+    printf ( " /// Departamento de Computação e Tecnologia                                          /// \n " );
+    printf ( " /// Disciplina DCT1106 - Programação                                                 /// \n " );
+    printf ( " /// Projeto Tarot da Cigana Alzira                                                   /// \n " );
+    printf ( " /// Lecionado por @flgorgonio - Out, 2021                                            /// \n " );
+    printf ( " /// Desenvolvido por @gcileno & @Gabriel5z - Out, 2021                               /// \n " );
     printf ( " ///                                                                                  /// \n " );
-    printf ( " /// Programa exemplo utilizado na disciplina DCT1106 - Programação, para /// \ n " );
-    printf ( " /// fins didáticos de ilustração. O programa contém os principais módulos /// \ n " );
-    printf ( " /// e funcionalidades que serão exigidos na avaliação dos projetos a serem /// \ n " );
-    printf ( " /// desenvolvidos pelos alunos ao longo da disciplina. Servir como um guia /// \ n " );
-    printf ( " /// de consulta e referência para os projetos desenvolvidos demais. /// \ n " );
+    printf ( " /// = = = = = = = = = = = = = Sistema Tarot da Cigana Alzira = = = = = = = = = = = = /// \n " );
+    printf ( " ///                                                                                  /// \n " );
+    printf ( " /// Programa exemplo utilizado na disciplina DCT1106 - Programação, para             /// \n " );
+    printf ( " /// fins didáticos de ilustração. O programa contém os principais módulos            /// \n " );
+    printf ( " /// e funcionalidades que serão exigidos na avaliação dos projetos a serem           /// \n " );
+    printf ( " /// desenvolvidos pelos alunos ao longo da disciplina. Servir como um guia           /// \n " );
+    printf ( " /// de consulta e referência para os projetos desenvolvidos demais.                  /// \n " );
     printf ( " ///                                                                                  /// \n " );
     printf ( " ///////////////////////////////////////////////// ////////////////////////////////////// \n " );
-    printf ( " \ n " );
+    printf ( " \n " );
     printf ( " ///////////////////////////////////////////////// ////////////////////////////////////// \n " );
     printf ( " ///                                                                                  /// \n " );
-    printf ( " /// = = = = = Sistema Tarot da Cigana Alzira = = = = = /// \n " );
+    printf ( " /// = = = = = = = = = = = = = Sistema Tarot da Cigana Alzira = = = = = = = = = = = = /// \n " );
     printf ( " ///                                                                                  /// \n " );
-    printf ( " /// Este projeto exemplo foi desenvolvido por: @gcileno &  @Gabriel5z /// \n " );
+    printf ( " /// Este projeto exemplo foi desenvolvido por: @gcileno &  @Gabriel5z                /// \n " );
     printf ( " ///                                                                                  /// \n " );
-    printf ( " /// E-mail: gabriel.cileno.093@ufrn.edu.br /// \ n " );
-    printf ( " /// Repositório: https://github.com/gcileno/Aninha-Uma-Cigana-Virtual.git  /// \n " );
+    printf ( " /// E-mail: gabriel.cileno.093@ufrn.edu.br                                           /// \n " );
+    printf ( " /// Repositório: https://github.com/gcileno/Aninha-Uma-Cigana-Virtual.git            /// \n " );
     printf ( " ///                                                                                  /// \n " );
     printf ( " ///////////////////////////////////////////////// ////////////////////////////////////// \n " );
     printf ( " \n " );
