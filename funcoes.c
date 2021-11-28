a054c1acb6077e769eccd3431fb96825d4e68cd7
gcileno <85129086+gcileno@users.noreply.github.com>
2021-11-28 20:14:11 -0300

    correção do modulo cadastro para o modulo funçao

--- funcoes.c
+++ funcoes.c
@@ -1,4 +1,5 @@
 void limpar (void);
+int cadastro(void);
 
 void limpar(void) {
   if (system("clear") || system("cls")) {
@@ -6,3 +7,19 @@ void limpar(void) {
     // adaptado de flavius
   }
 }
+
+int cadastro (void) {
+    int dia, mes;
+    char nome[200];
+    printf("Digite seu nome: \n");
+    scanf("%s",nome);
+    printf(" Digite o dia de seu nascimento:   \n");
+    scanf("%d", &dia); 
+    printf(" Digite o mês de seu nascimento:   \n");
+    scanf("%d", &mes);
+    printf("Olá, %s ", nome);
+    printf("seu signo é: ");
+    telalogin();
+
+    return (0);
+};
