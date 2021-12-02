ee4a8797d0fe4ea5f32ee52f0b06cc9d12e92f50
gcileno <85129086+gcileno@users.noreply.github.com>
2021-12-02 10:31:42 -0300

    entrada das cartas e seus significados

---
+++ cartas.c
@@ -0,0 +1,38 @@
+void telaSobre(void);
+void telaPrincipal(void);
+void telaEquipe(void);
+void telaCadastro(void);
+void telaEscolhacarta(void);
+void telareusltado(void);
+void baralho(void);
+void limpar (void);
+void cadastro(void);
+void telalogin(void);
+
+char cartas [22][15][2048] = {
+  {"O Mago: ", "O arcano maior chamado “O Mago” brotou do Tarot como um conselho para este particular
+    momento, gabriel. Ele vem sugerir a necessidade de uma quebra de rotina, experimentação de novas ideias,
+    fazer diferente, tentar um outro curso de ação. Este é um momento propício para encontros inesperados e
+    conversas libertadoras. A qualidade do momento demanda espanar a poeira e abrir-se ao novo.
+    Muito estímulo intelectual lhe aguarda nesta etapa de sua vida, o que pode surgir na forma de livros
+    que lhe são indicados, pessoas que surgem do nada pra bater um papo com você e lhe permitir um maior
+    entendimento das coisas. É como se, agora, as coisas que você fosse ouvir (provavelmente da boca de
+    gente que você nem esperava!) lhe permitissem uma libertação de uma forma antiga e superada de 
+    abordar as situações. Aproveite este dinâmico período!Conselho: Momento de renovar, de fazer 
+    diferente."},
+
+  {"O Imperador","O conselho emitido pelo Tarot vem através da imagem do arcano IV, chamado “O Imperador”,
+    cuja imagem nos revela uma figura masculina solidamente colocada, irradiando poder e autoridade.
+    O pedido do arcano IV é o da importância de reconhecer a própria força e não depender demais de 
+    ninguém. Sempre que dependemos do outro, o outro pode falhar conosco eventualmente e qualquer 
+    felicidade excessivamente buscada fora de nós é absolutamente temporária.Procure neste momento,
+    cultivar a referência do seu próprio poder pessoal e não se deixe levar demais pelos conselhos alheios.
+    Reconheça, em si, a autoridade para comandar sua própria vida.Conselho: Seja mais independente neste
+    momento"},
+
+  {"A Força", "A ideia que esta carta traduz é a da necessidade, neste momento, de ter controle sobre 
+  suas próprias raivas e paixões, controlando os instintos mais primitivos.Muitas vezes queremos vencer 
+  obstáculos externos, obter vitórias, mas tal coisa só é possível quando controlamos a nós mesmos com a 
+  devida firmeza, afinal de contas a verdadeira força é aquela que exercemos sobre nós mesmos.A despeito 
+  de todas as tentações que você terá para agir como uma criança mimada, será cobrada de você uma postura 
+  adulta.Conselho: Procure lidar melhor com a própria raiva."},
---
+++ cartas.h
@@ -0,0 +1,10 @@
+void telaSobre(void);
+void telaPrincipal(void);
+void telaEquipe(void);
+void telaCadastro(void);
+void telaEscolhacarta(void);
+void telareusltado(void);
+void baralho(void);
+void limpar (void);
+void cadastro(void);
+void telalogin(void);
