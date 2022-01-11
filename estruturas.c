98b74df3ba871a488906f39ec8d81b259572d1d1
gcileno <gabriel.cileno.093@ufrn.edu.br>
2022-01-10 21:04:57 -0300

    definição de estruturas

---
+++ estruturas.c
@@ -0,0 +1,42 @@
+// definição da estrutura do cadastro do usuraio e seus dados.
+
+struct Usuario
+{
+    /* data */
+    char acesso;
+    char nome[20];
+    int dNas;
+    int dMes;
+    char signo;
+    int ultCarta;
+    
+};
+
+struct Relatorio
+{
+    /* data */
+    int cartasTiradas;
+    int cartasMes;
+    int cartasDia;
+
+};
+
+struct Cigana
+{
+    /* data */
+    int usur;
+    int nome;
+};
+
+struct Baralho
+{
+    /* data */
+    char carta;
+    char desc;
+};
+
+
+
+
+
+
