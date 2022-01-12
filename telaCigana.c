921c70fb6df7d243dbe10657ee830bb99888019a
gcileno <85129086+gcileno@users.noreply.github.com>
2022-01-12 16:47:04 -0300

    correção

---
+++ telaCigana.c
@@ -0,0 +1,19 @@
+// tela para o modulo da cigana
+void telaCigana(void);
+
+void telaCigana(){
+    limpar();
+    printf("\n");
+
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Sistema de Tarot da cigana Alzira = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Relatório de atendimento                                  ///\n");
+    printf("///            2. Relatório de cartas tiradas                               ///\n");
+    printf("///            3. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
